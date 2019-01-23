4fea19f4bb3b9627534ecb591d0f32d18ea9cd90
Choirotunnisa <choichoi878@gmail.com>
2019-01-23 14:03:02 +0700

    s

--- application/views/spbefix/contentadmin/listnomenklatur.php
+++ application/views/spbefix/contentadmin/listnomenklatur.php
@@ -1,11 +1,10 @@
-page content -->
+<-- page content -->
 <div class="right_col" role="main">
 	<div class="">
 		<div class="page-title">
 			<div class="title_left">
 				<h3><small></small></h3>
 			</div>
-
 			<div class="title_right">
 				<div class="col-md-5 col-sm-5 col-xs-12 form-group">
 					<!-- <div class="input-group">
@@ -17,9 +16,7 @@ page content -->
 				</div>
 			</div>
 		</div>
-
 		<div class="clearfix"></div>
-
 		<div class="row">
 			<div class="col-md-12 col-sm-12 col-xs-12">
 				<div class="x_panel">
