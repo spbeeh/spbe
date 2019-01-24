d41e4ebb791a68f05db20d351358ad85cd8e5090
qodirtok <qodirtok@gmail.com>
2019-01-24 22:18:02 +0700

    asd

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,50 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login V18</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?php echo site_url('Asset/images/icons/favicon.ico') ?>"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=site_url('Asset/vendor/bootstrap/css/bootstrap.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') ?>">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?php echo site_url('Asset/images/icons/favicon.ico') ?>" />
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= site_url('Asset/vendor/bootstrap/css/bootstrap.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo site_url('Asset/fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo site_url('Asset/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') ?>">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/vendor/animate/animate.css') ?>">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/vendor/css-hamburgers/hamburgers.min.css') ?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/vendor/animsition/css/animsition.min.css') ?>">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo site_url('Asset/vendor/css-hamburgers/hamburgers.min.css') ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo site_url('Asset/vendor/animsition/css/animsition.min.css') ?>">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/vendor/select2/select2.min.css') ?>">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/vendor/daterangepicker/daterangepicker.css') ?>">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?php echo site_url('Asset/vendor/daterangepicker/daterangepicker.css') ?>">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/css/util.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('Asset/css/main.css') ?>">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body style="background-color: #666666;">
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-43">
-						Login to continue
+						Login
 					</span>
-					
-					
-					<div class="wrap-input100 validate-input" data-validate = "number">
+
+
+					<div class="wrap-input100 validate-input" data-validate="number">
 						<input class="input100" type="text" name="number">
 						<span class="focus-input100"></span>
 						<span class="label-input100">NIP</span>
 					</div>
-					
-					
+
+
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass">
 						<span class="focus-input100"></span>
@@ -65,48 +72,50 @@
 							</a>
 						</div>
 					</div>
-			
+
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" a href="<?php echo site_url('menu/dashboard') ?>" >
+						<button class="login100-form-btn" a href="<?php echo site_url('menu/dashboard') ?>">
 							Login </a></button>
 
 					</div>
-					
+
 					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
-							or sign up using
+							<!-- or sign up using -->
 						</span>
 					</div>
 				</form>
 
-				<div class="login100-more" style="background-image: url('<?php echo base_url('Asset/images/teratai.jpg') ?>');">
-					
+				<div class="login100-more"
+					style="background-image: url('<?php echo base_url('Asset/images/teratai.jpg') ?>');">
+
 				</div>
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-	
-<!--===============================================================================================-->
+
+
+
+
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Assets/vendor/jquery/jquery-3.2.1.min.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Assets/vendor/animsition/js/animsition.min.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Asset/vendor/bootstrap/js/popper.js') ?>"></script>
 	<script src="<?php echo base_url('Asset/vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Asset/vendor/select2/select2.min.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Asset/vendor/daterangepicker/moment.min.js') ?>"></script>
 	<script src="<?php echo base_url('Asset/vendor/daterangepicker/daterangepicker.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Asset/vendor/countdowntime/countdowntime.js') ?>"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?php echo base_url('Asset/js/main.js') ?>"></script>
 
 </body>
+
 </html>
