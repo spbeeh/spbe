27e422ab8fd7e4832a6ca53894aa2949c537e270
Choirotunnisa <choichoi878@gmail.com>
2019-01-29 11:17:11 +0700

    qq

--- application/views/menu/contect/hal1.php
+++ application/views/menu/contect/hal1.php
@@ -18,143 +18,58 @@
 	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div class="x_panel">
 			<div class="x_content">
-
-				<!-- Smart Wizard -->
-
-				<div id="wizard" class="form_wizard wizard_horizontal">
-					<ul class="wizard_steps">
-						<li>
-							<a href="#step-1">
-								<span class="step_no">1</span>
-								<span class="step_descr">
-									Step 1<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-2">
-								<span class="step_no">2</span>
-								<span class="step_descr">
-									Step 2<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-3">
-								<span class="step_no">3</span>
-								<span class="step_descr">
-									Step 3<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-4">
-								<span class="step_no">4</span>
-								<span class="step_descr">
-									Step 4<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-5">
-								<span class="step_no">5</span>
-								<span class="step_descr">
-									Step 5<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-6">
-								<span class="step_no">6</span>
-								<span class="step_descr">
-									Step 6<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-7">
-								<span class="step_no">7</span>
-								<span class="step_descr">
-									Step 7<br />
-								</span>
-							</a>
-						</li>
-						<li>
-							<a href="#step-8">
-								<span class="step_no">8</span>
-								<span class="step_descr">
-									Step 8<br />
-								</span>
-							</a>
-						</li>
-					</ul>
-
-					<div id="step-1">
-						<div class="row">
-							<div class="col-md-12 col-sm-12 col-xs-12">
-								<div class="x_panel">
-									<div class="x_title">
-										<font size="4">Jelaskan misi/sasaran pelaksanaan SPBE di Instansi
-											Pusat/Pemerintah Daerah masing-masing?</font>
-										<div class="clearfix"></div>
-									</div>
-									<div class="x_content">
-										<br />
-										<form id="demo-form2" data-parsley-validate
-											class="form-horizontal form-label-left">
-
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="x_panel">
+							<div class="x_title">
+								<font size="4">1. Jelaskan misi/sasaran pelaksanaan SPBE di Instansi Pusat/Pemerintah Daerah masing-masing?</font>
+							<div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<br />
+									<form id="demo-form2" data-parsley-validate
+										class="form-horizontal form-label-left">
 											<div class="form-group">
-												<textarea id="message" required="required" class="form-control"
-													name="message"
-													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+												<textarea id="message" required="required" class="form-control" name="message" style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
 											</div>
-										</form>
-									</div>
-								</div>
+									</form>
 							</div>
 						</div>
 					</div>
+				</div>
 
-					<div id="step-2">
-						<div class="row">
-							<div class="col-md-12 col-sm-12 col-xs-12">
-								<div class="x_panel">
-									<div class="x_title">
-										<font size="4">Lampirkan daftar anggota Tim Evaluator Internal di Instansi
-											Pusat/Pemerintah Daerah masing-masing, termasuk surat keputusan
-											pembentukan Tim Evaluator Internal jika ada.</font>
-										<div class="clearfix"></div>
-									</div>
-									<div class="x_content">
-										<br />
-										<form id="demo-form2" data-parsley-validate
-											class="form-horizontal form-label-left">
-
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="x_panel">
+							<div class="x_title">
+								<font size="4">2. Lampirkan daftar anggota Tim Evaluator Internal di Instansi Pusat/Pemerintah Daerah masing-masing, termasuk surat keputusan pembentukan Tim Evaluator Internal jika ada.</font>
+							<div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<br />
+									<form id="demo-form2" data-parsley-validate
+										class="form-horizontal form-label-left">
 											<div class="form-group">
-												<textarea id="message" required="required" class="form-control"
-													name="message"
-													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+												<textarea id="message" required="required" class="form-control" name="message" style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
 											</div>
-										</form>
-									</div>
-								</div>
+									</form>
 							</div>
 						</div>
 					</div>
+				</div>
 
-					<div id="step-3">
-						<div class="row">
-							<div class="col-md-12 col-sm-12 col-xs-12">
-								<div class="x_panel">
-									<div class="x_title">
-										<font size="4">Pertanyaan khusus Pemerintah Daerah</font>
-										<div class="clearfix"></div>
-									</div>
-									<div class="x_content">
-										<br />
-										<form id="demo-form2" data-parsley-validate
-											class="form-horizontal form-label-left">
-											<font size="4">Sebutkan perguruan tinggi negeri maupun swasta yang
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="x_panel">
+							<div class="x_title">
+								<font size="4">3. Pertanyaan khusus Pemerintah Daerah</font>
+							<div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<br />
+									<form id="demo-form2" data-parsley-validate
+										class="form-horizontal form-label-left">
+											<font size="4">a. Sebutkan perguruan tinggi negeri maupun swasta yang
 												memiliki jurusan/program studi terkait komputer, teknologi informasi
 												dan komunikasi, sistem informasi, atau sejenisnya di wilayah
 												Pemerintah Daerah masing-masing?<br></font>
@@ -164,7 +79,7 @@
 													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
 											</div>
 
-											<font size="4">Berapakah persentase ketersediaan sumber daya listrik
+											<font size="4">b. Berapakah persentase ketersediaan sumber daya listrik
 												untuk pelaksanaan SPBE di Pemerintah Daerah masing-masing dalam satu
 												bulan?<br></font>
 											<div class="form-group">
@@ -178,7 +93,7 @@
 												dengan cara: (jumlah jam sumber daya listrik hidup) dibagi dengan
 												(jumlah jam dalam 30 hari) dikali dengan 100%.</font><br /><br />
 
-											<font size="4">Jika ada, jelaskan hubungan kerja dengan konsultan,
+											<font size="4">c. Jika ada, jelaskan hubungan kerja dengan konsultan,
 												perusahaan di bidang TIK, komunitas TIK, perguruan tinggi atau pihak
 												lain yang membantu dalam pelaksanaan SPBE di Pemerintah Daerah
 												masing-masing.<br></font>
@@ -194,28 +109,25 @@
 												jaringan, pengoperasian pusat data, pengolahan data, dan lainnya
 												dengan menyebutkan tahun dan durasi hubungan kerja tersebut.<br>
 											</font>
-
 										</form>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div id="step-4">
 						<div class="row">
 							<div class="col-md-12 col-sm-12 col-xs-12">
 								<div class="x_panel">
 									<div class="x_title">
-										<font size="4">Apakah terdapat penanggung jawab terhadap pelaksanaan SPBE di
+										<font size="4">4. Apakah terdapat penanggung jawab terhadap pelaksanaan SPBE di
 											Instansi Pusat/Pemerintah Daerah masing-masing? Jika ada, sebutkan.
 										</font>
-										<div class="clearfix"></div>
+									<div class="clearfix"></div>
 									</div>
 									<div class="x_content">
 										<br />
-										<form id="demo-form2" data-parsley-validate
-											class="form-horizontal form-label-left">
+											<form id="demo-form2" data-parsley-validate
+												class="form-horizontal form-label-left">
 											<div class="form-group">
 												<textarea id="message" required="required" class="form-control"
 													name="message"
@@ -232,14 +144,12 @@
 								</div>
 							</div>
 						</div>
-					</div>
-
-					<div id="step-5">
+					
 						<div class="row">
 							<div class="col-md-12 col-sm-12 col-xs-12">
 								<div class="x_panel">
 									<div class="x_title">
-										<font size="4">Sebutkan unit kerja/perangkat daerah yang terlibat dalam
+										<font size="4">5. Sebutkan unit kerja/perangkat daerah yang terlibat dalam
 											pelaksanaan SPBE (perencanaan, penerapan, pengoperasian, pemantauan dan
 											evaluasi SPBE) dan lampirkan struktur organisasinya.</font>
 										<div class="clearfix"></div>
@@ -253,13 +163,100 @@
 													name="message"
 													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
 											</div>
+											</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="x_panel">
+							<div class="x_title">
+								<font size="4">6. Sebutkan nilai anggaran untuk pelaksanaan SPBE khususnya pada bidang teknologi informasi dan komunikasi di Instansi Pusat/Pemerintah Daerah masing-masing pada tahun 2017 sebagai berikut: </font>
+							<div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<br />
+									<form id="demo-form2" data-parsley-validate
+										class="form-horizontal form-label-left">
+											<font size="4">a. Pembangunan/pengembangan/pembelian aplikasi dan software komersial.<br></font>
+											<div class="form-group">
+												<textarea id="message" required="required" class="form-control"
+													name="message"
+													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+											</div>
+
+											<font size="4">b. Pembelian/pengadaan perangkat keras teknologi informasi dan komunikasi antara lain PC, laptop, server, penyimpan data (storage), jaringan, appliance, dan pendukung lainnya.<br></font>
+											<div class="form-group">
+												<textarea id="message" required="required" class="form-control"
+													name="message2"
+													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+											</div>
+
+											<font size="4">c. Pemeliharaan perangkat keras teknologi informasi dan komunikasi, aplikasi, dan software komersial antara lain perawatan PC/laptop/server/jaringan/appliance, lisensi software komersial, bandwidth internet, jasa sewa perangkat, jasa pengoperasian, dan jasa konsultansi lainnya.<br></font>
+											<div class="form-group">
+												<textarea id="message" required="required" class="form-control"
+													name="message3"
+													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+											</div>
 										</form>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
+
+						<div class="row">
+							<div class="col-md-12 col-sm-12 col-xs-12">
+								<div class="x_panel">
+									<div class="x_title">
+										<font size="4">7. Berapakah persentase nilai anggaran untuk pelaksanaan SPBE khususnya pada bidang teknologi informasi dan komunikasi terhadap total nilai anggaran Instansi Pusat/Pemerintah Daerah masing-masing pada tahun 2017. 
+										</font>
+									<div class="clearfix"></div>
+									</div>
+									<div class="x_content">
+										<br />
+											<form id="demo-form2" data-parsley-validate
+												class="form-horizontal form-label-left">
+											<div class="form-group">
+												<textarea id="message" required="required" class="form-control"
+													name="message"
+													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+											</div>
+
+											<font size="4">Penjelasan : <br></font>
+											<font size="4">Nilai anggaran untuk pelaksanaan SPBE khususnya pada bidang teknologi informasi dan komunikasi adalah penjumlahan dari pertanyaan 6.a, 6.b, dan 6.c.<br></font>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-12 col-sm-12 col-xs-12">
+								<div class="x_panel">
+									<div class="x_title">
+										<font size="4">8. 8.	Buatlah daftar aplikasi yang dipergunakan untuk melaksanakan manajemen internal birokrasi dan pelayanan publik di Instansi Pusat/Pemerintah Daerah masing-masing.
+										</font>
+									<div class="clearfix"></div>
+									</div>
+									<div class="x_content">
+										<br />
+											<form id="demo-form2" data-parsley-validate
+												class="form-horizontal form-label-left">
+											<div class="form-group">
+												<textarea id="message" required="required" class="form-control"
+													name="message"
+													style="margin: 0px -5.5px 0px 0px; width: 100%; height: 140px; resize: none;"></textarea>
+											</div>
+
+											<font size="4">Penjelasan : <br></font>
+											<font size="4">Daftar aplikasi menggunakan formulir yang telah ditetapkan.<br></font>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
 			</div>
 			<!-- /.row -->
 		</div>
