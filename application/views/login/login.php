9c6cf73088b0a0643c0ccb50d1650f1e841f3099
Choirotunnisa <choichoi878@gmail.com>
2019-01-30 15:01:22 +0700

    qw

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -2,100 +2,92 @@
 <html lang="en">
 
 <head>
-	<title><?php echo ucfirst($this->uri->segment(1)) ?></title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="<?php echo base_url('Asset/images/icons/favicon.ico') ?>" />
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/vendor/bootstrap/css/bootstrap.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('Asset/vendor/animate/animate.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/vendor/css-hamburgers/hamburgers.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/vendor/animsition/css/animsition.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('Asset/vendor/select2/select2.min.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css"
-		href="<?php echo base_url('Asset/vendor/daterangepicker/daterangepicker.css') ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('Asset/css/util.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('Asset/css/main.css') ?>">
-	<!--===============================================================================================-->
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="<php echo base_url('Asset/assets/img/apple-icon.png') ?>">
+<link rel="icon" type="image/png" href="<?php echo base_url('Asset/images/icons/favicon.ico') ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title><?php echo ucfirst($this->uri->segment(1)) ?></title>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+<!-- CSS Files -->
+<link href="<?php echo base_url('Asset/assets/css/bootstrap.min.css') ?>" rel="stylesheet" />
+<link href="<?php echo base_url('Asset/assets/css/now-ui-kit.css?v=1.2.0')?>" rel="stylesheet" />
+<!-- CSS Just for demo purpose, don't include it in your project -->
+<link href="<?php echo base_url('Asset/assets/demo/demo.css') ?>" rel="stylesheet" />
 </head>
 
-<body style="background-color: #666666;">
+<body class="login-page sidebar-collapse">
+<!-- Navbar -->
+<!-- End Navbar -->
+<div class="page-header clear-filter" filter-color="orange">
+<div class="page-header-image" style="background-image:url(<?php echo base_url('Asset/assets/img/login.jpg') ?>"></div>
+<div class="content">
+<div class="container">
+<div class="col-md-4 ml-auto mr-auto">
+<div class="card card-login card-plain">
+<form class="form" method="Post" action="<?php echo base_url('login') ?>"
+role="login">
+<div class="card-header text-center">
+<div class="logo-container">
+<img src="<?php echo base_url('Asset/assets/img/now-logo.png') ?>" alt="">
+</div>
+</div>
+<div class="card-body">
+<div class="input-group no-border input-lg" data-validate="number">
+<div class="input-group-prepend">
+<span class="input-group-text">
+<i class="now-ui-icons users_circle-08"></i>
+</span>
+</div>
+<input type="text" class="form-control" name="nip_pj" placeholder="NIP">
+</div>
+<div class="input-group no-border input-lg" data-validate="Password is required">
+<div class="input-group-prepend">
+<span class="input-group-text">
+<i class="now-ui-icons text_caps-small"></i>
+</span>
+</div>
+<input type="password" placeholder="Password" name="password" class="form-control" />
+</div>
+</div>
+<div class="card-footer text-center">
+<button type="submit" class="btn btn-primary btn-round btn-lg btn-block" name="submit" value="login">Login</button>
 
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form" method="Post" action="<?php echo base_url('login') ?>"
-					role="login">
-					<span class="login100-form-title p-b-43">
-						SPBE
-					</span>
-
-					<div class="wrap-input100 validate-input" data-validate="number">
-						<input class="input100" type="text" name="nip_pj">
-						<span class="focus-input100"></span>
-						<span class="label-input100">NIP</span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="password">
-						<span class="focus-input100"></span>
-						<span class="label-input100">Password</span>
-					</div>
-
-					<div class="container-login100-form-btn">
-						<button type="submit" class="login100-form-btn" name="submit" value="login">Login
-						</button>
-					</div>
-					<div class="text-center p-t-46 p-b-20">
-						<span class="txt2">
-							<!-- or sign up using -->
-						</span>
-					</div>
-				</form>
-				<div class="login100-more"
-					style="background-image: url('<?php echo base_url('Asset/images/teratai.jpg') ?>');">
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-
-
-
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/jquery/jquery-3.2.1.min.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/animsition/js/animsition.min.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/bootstrap/js/popper.js') ?>"></script>
-	<script src="<?php echo base_url('Asset/vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/select2/select2.min.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/daterangepicker/moment.min.js') ?>"></script>
-	<script src="<?php echo base_url('Asset/vendor/daterangepicker/daterangepicker.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/vendor/countdowntime/countdowntime.js') ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('Asset/js/main.js') ?>"></script>
-
-</body>
-
-</html>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- <footer class="footer">
+<div class="container">
+<div class="copyright" id="copyright">
+&copy;
+<script>
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+</script>, Designed by
+<a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
+<a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+</div>
+</div>
+</footer>
+</div> -->
+<!--   Core JS Files   -->
+<script src="<?php echo base_url('Asset/assets/js/core/jquery.min.js')?>" type="text/javascript"></script>
+<script src="<?php echo base_url('Asset/assets/js/core/popper.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('Asset/assets/js/core/bootstrap.min.js') ?>" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+	<script src="<?php echo base_url('Asset/assets/js/plugins/bootstrap-switch.js') ?>"></script>
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+		<script src="<?php echo base_url('Asset/assets/js/plugins/nouislider.min.js') ?>" type="text/javascript"></script>
+		<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+			<script src="<?php echo base_url('Asset/assets/js/plugins/bootstrap-datepicker.js') ?>" type="text/javascript"></script>
+			<!--  Google Maps Plugin    -->
+			<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+			<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+			<script src="<?php echo base_url('Asset/assets/js/now-ui-kit.js?v=1.2.0') ?>" type="text/javascript"></script>
+			</body>
+			
+			</html>
